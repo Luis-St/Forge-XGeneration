c2c92d2cb93d4eb3dcde87e8f571aef9031164d8
BuildTools <76595940+Luis-st@users.noreply.github.com>
2021-04-15 18:21:59 +0200

    modify the pipe block

--- src/main/java/net/luis/industry/api/tileentity/RecipeTileEntity.java
+++ src/main/java/net/luis/industry/api/tileentity/RecipeTileEntity.java
@@ -1,13 +1,12 @@
-package net.luis.industry.api.tileentities;
+package net.luis.industry.api.tileentity;
 
 import net.luis.industry.api.recipe.IModRecipe;
 import net.luis.industry.api.recipe.IModRecipeHelper;
-import net.luis.industry.api.recipe.util.RecipeList;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 
-public class RecipeTileEntity extends TileEntity implements IRecipeProvider<IModRecipe>, ITickableTileEntity {
+public class RecipeTileEntity extends TileEntity implements ITickableTileEntity {
 
 	public RecipeTileEntity(TileEntityType<?> type, IModRecipe recipe, IModRecipeHelper<IModRecipe> recipeHelper) {
 		
@@ -19,20 +18,6 @@ public class RecipeTileEntity extends TileEntity implements IRecipeProvider<IMod
 	public void tick() {
 		
 		
-		
-	}
-
-	@Override
-	public RecipeList<IModRecipe> getRecipeList() {
-		
-		return null;
-		
-	}
-
-	@Override
-	public IModRecipeHelper<IModRecipe> getRecipeHelper() {
-		
-		return null;
 		
 	}
 
--- src/main/java/net/luis/industry/common/block/fluid/PipeBlock.java
+++ src/main/java/net/luis/industry/common/block/fluid/PipeBlock.java
@@ -1,15 +1,23 @@
 package net.luis.industry.common.block.fluid;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import net.luis.industry.init.util.tags.ModBlockTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SixWayBlock;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.pathfinding.PathType;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 
@@ -18,6 +26,8 @@ public class PipeBlock extends SixWayBlock {
 	public PipeBlock(Properties properties) {
 		
 		super(0.25F, properties);
+		this.setDefaultState(this.stateContainer.getBaseState().with(DOWN, Boolean.valueOf(false)).with(UP, Boolean.valueOf(false))
+				.with(NORTH, Boolean.valueOf(false)).with(EAST, Boolean.valueOf(false)).with(SOUTH, Boolean.valueOf(false)).with(WEST, Boolean.valueOf(false)));
 		
 	}
 	
@@ -47,12 +57,23 @@ public class PipeBlock extends SixWayBlock {
 	}
 	
 	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		
+		VoxelShape pipeShape = super.getShape(state, worldIn, pos, context);
+		VoxelShape dispatcherShape = VoxelShapes.combineAndSimplify(Block.makeCuboidShape(2, 2, 2, 14, 14, 14), pipeShape, IBooleanFunction.OR);
+		List<BooleanProperty> properties = FACING_TO_PROPERTY_MAP.values().stream().collect(Collectors.toList());
+		properties.removeIf(property -> !state.get(property));
+		
+		return properties.size() >= 3 ? dispatcherShape : pipeShape;
+	}
+	
+	@Override
 	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
 		
 		boolean flag = PipeBlock.isAllowedConnection(facingState.getBlock());
 		
 		return stateIn.with(FACING_TO_PROPERTY_MAP.get(facing), Boolean.valueOf(flag));
-		
+			
 	}
 	
 	public static boolean isAllowedConnection(Block block) {
