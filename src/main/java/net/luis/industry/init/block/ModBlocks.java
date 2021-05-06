04f2a47e7317defb9413c37e11a9d56c92021418
BuildTools <76595940+Luis-st@users.noreply.github.com>
2021-05-06 10:40:34 +0200

    add deepslate recipes and loottables

--- src/main/java/net/luis/industry/init/block/ModBlocks.java
+++ src/main/java/net/luis/industry/init/block/ModBlocks.java
@@ -2,14 +2,18 @@ package net.luis.industry.init.block;
 
 import static net.luis.industry.Industry.MOD_ID;
 
+import net.luis.industry.api.block.ModOreBlock;
 import net.luis.industry.common.block.fluid.PipeBlock;
 import net.luis.industry.common.block.mechanical.MilestoneBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.RedstoneOreBlock;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.block.StairsBlock;
 import net.minecraft.block.WallBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -20,16 +24,16 @@ public class ModBlocks {
 	
 	
 	public static final RegistryObject<PipeBlock> FLUID_PIPE = BLOCKS.register("fluid_pipe", 
-			() -> new PipeBlock(Block.Properties.of(Material.METAL, MaterialColor.METAL).harvestLevel(2)
-					.strength(4.0F, 10.0F).requiresCorrectToolForDrops()));
+			() -> new PipeBlock(Block.Properties.of(Material.METAL, MaterialColor.METAL).harvestLevel(1)
+					.strength(4.0F, 10.0F).requiresCorrectToolForDrops().harvestTool(ToolType.PICKAXE)));
 	
 	public static final RegistryObject<MilestoneBlock> MILESTONE = BLOCKS.register("milestone",
-			() -> new MilestoneBlock(Block.Properties.of(Material.STONE, MaterialColor.STONE).harvestLevel(2)
-					.strength(3.5F, 6.0F).requiresCorrectToolForDrops()));
+			() -> new MilestoneBlock(Block.Properties.of(Material.STONE, MaterialColor.STONE).harvestLevel(1)
+					.strength(3.5F, 6.0F).requiresCorrectToolForDrops().harvestTool(ToolType.PICKAXE)));
 	
 	public static final RegistryObject<Block> DEEPSLATE = BLOCKS.register("deepslate", 
 			() -> new Block(Block.Properties.of(Material.STONE, MaterialColor.STONE).requiresCorrectToolForDrops()
-					.strength(2.0F, 6.5F)));
+					.strength(2.0F, 6.5F).harvestTool(ToolType.PICKAXE)));
 	
 	public static final RegistryObject<Block> COBBLED_DEEPSLATE = BLOCKS.register("cobbled_deepslate", 
 			() -> new Block(Block.Properties.copy(ModBlocks.DEEPSLATE.get())));
@@ -97,5 +101,32 @@ public class ModBlocks {
 	public static final RegistryObject<SlabBlock> DEEPSLATE_TILE_SLAB = BLOCKS.register("deepslate_tile_slab", 
 			() -> new SlabBlock(Block.Properties.copy(ModBlocks.DEEPSLATE.get())));
 	
+	public static final RegistryObject<Block> COPPER_ORE = BLOCKS.register("copper_ore", 
+			() -> new Block(Block.Properties.copy(Blocks.IRON_ORE)));
+	
+	public static final RegistryObject<ModOreBlock> DEEPSLATE_COAL_ORE = BLOCKS.register("deepslate_coal_ore", 
+			() -> new ModOreBlock(Block.Properties.of(Material.STONE, MaterialColor.COLOR_BLACK)
+					.requiresCorrectToolForDrops().harvestLevel(2).strength(4.0F, 7.0F).harvestTool(ToolType.PICKAXE)));
+	
+	public static final RegistryObject<Block> DEEPSLATE_IRON_ORE = BLOCKS.register("deepslate_iron_ore", 
+			() -> new Block(Block.Properties.copy(ModBlocks.DEEPSLATE_COAL_ORE.get())));
+	
+	public static final RegistryObject<Block> DEEPSLATE_COPPER_ORE = BLOCKS.register("deepslate_copper_ore", 
+			() -> new Block(Block.Properties.copy(ModBlocks.DEEPSLATE_COAL_ORE.get())));
+	
+	public static final RegistryObject<Block> DEEPSLATE_GOLD_ORE = BLOCKS.register("deepslate_gold_ore", 
+			() -> new Block(Block.Properties.copy(ModBlocks.DEEPSLATE_COAL_ORE.get())));
+	
+	public static final RegistryObject<ModOreBlock> DEEPSLATE_LAPIS_ORE = BLOCKS.register("deepslate_lapis_ore", 
+			() -> new ModOreBlock(Block.Properties.copy(ModBlocks.DEEPSLATE_COAL_ORE.get())));
+	
+	public static final RegistryObject<RedstoneOreBlock> DEEPSLATE_REDSTONE_ORE = BLOCKS.register("deepslate_redstone_ore", 
+			() -> new RedstoneOreBlock(Block.Properties.copy(ModBlocks.DEEPSLATE_COAL_ORE.get())));
+	
+	public static final RegistryObject<ModOreBlock> DEEPSLATE_DIAMOND_ORE = BLOCKS.register("deepslate_diamond_ore", 
+			() -> new ModOreBlock(Block.Properties.copy(ModBlocks.DEEPSLATE_COAL_ORE.get())));
+	
+	public static final RegistryObject<ModOreBlock> DEEPSLATE_EMERALD_ORE = BLOCKS.register("deepslate_emerald_ore", 
+			() -> new ModOreBlock(Block.Properties.copy(ModBlocks.DEEPSLATE_COAL_ORE.get())));
 	
 }
