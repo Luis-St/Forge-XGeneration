02dae8c2936e3daf78d9ac067db34c4281ca94fa
BuildTools <76595940+Luis-st@users.noreply.github.com>
2021-12-03 19:45:26 +0100

    remove Test Dimension

--- src/main/java/net/luis/nero/event/fml/OnCommonSetupEvent.java
+++ src/main/java/net/luis/nero/event/fml/OnCommonSetupEvent.java
@@ -8,8 +8,6 @@ import com.google.common.collect.Lists;
 import net.luis.nero.Nero;
 import net.luis.nero.common.world.biome.source.OverworldBiomeSource;
 import net.luis.nero.common.world.levelgen.OverworldChunkGenerator;
-import net.luis.nero.common.world.test.TestBiomeSource;
-import net.luis.nero.common.world.test.TestChunkGenerator;
 import net.luis.nero.init.entity.ModEntityTypes;
 import net.luis.nero.network.NetworkHandler;
 import net.minecraft.core.Registry;
@@ -58,8 +56,6 @@ public class OnCommonSetupEvent {
 	protected static void registerUtil(FMLCommonSetupEvent event) {
 		Registry.register(Registry.CHUNK_GENERATOR, new ResourceLocation(Nero.MOD_ID, "overworld_chunk_generator"), OverworldChunkGenerator.CODEC);
 		Registry.register(Registry.BIOME_SOURCE, new ResourceLocation(Nero.MOD_ID, "overworld_biome_source"), OverworldBiomeSource.CODEC);
-		Registry.register(Registry.CHUNK_GENERATOR, new ResourceLocation(Nero.MOD_ID, "test_chunk_generator"), TestChunkGenerator.CODEC);
-		Registry.register(Registry.BIOME_SOURCE, new ResourceLocation(Nero.MOD_ID, "test_biome_source"), TestBiomeSource.CODEC);
 	}
 	
 	@SuppressWarnings("unused")
